a634c168a77c30b8b4d05a2518794e582cf1686d
ALbert-Shen-code <siyuanshen1993@gmail.com>
2021-08-17 20:44:37 +1000

    practice updated

--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({filterItems, category}) => {
+  return<div className="btn-container">
+    {category.map((cate)=>(
+      <button className="filter-btn" onClick={()=>filterItems(cate)}>{cate}</button>
+    ))}
+
+  </div>;
 };
 
 export default Categories;
--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,45 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('');
+  const [error, seterror] = useState(false);
+  const [list, setList] = useState(new Values('#f15025').all(10));
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      seterror(true);
+      console.log(error);
+    }
+    
+  }
+  return (
+    <>
+    <section className="container">
+      <h4>Color generator</h4>
+      <form onSubmit={handleSubmit}>
+          <input type="text" value={color} onChange={(e) => setColor(e.target.value)} placeholder="#15025" 
+          className={`${error ? 'error' : null}`}/>
+        <button className="btn" type="submit">
+          Change color
+        </button>
+      </form>
+    </section>
+    <section className="colors">
+        {list.map((color,index)=>{
+          const hex = color.hex;
+          return (
+            <SingleColor key={index} {...color} index={index} hexColor={hex}/>
+          )
+          
+        })}
+      </section>
+    </>
+    
+  )
 }
 
 export default App
